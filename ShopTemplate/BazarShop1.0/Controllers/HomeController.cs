7293ff7b5b8b365e7ca0d1c3412088c5be40973e
Marcel_Klukiewicz <Marcel_Klukiewicz@bojkowski.it>
2025-07-11 15:10:00 +0200

    1 step of project

---
+++ ShopTemplate/BazarShop1.0/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using BazarShop1._0.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BazarShop1._0.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Home() => View();
+
+        public IActionResult Contact() => View();
+    }
+}
---
+++ ShopTemplate/BazarShop1.0/Controllers/InfoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BazarShop1._0.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Policy() => View();
+        public IActionResult Returns() => View();
+        public IActionResult Rights() => View();
+    }
+}
