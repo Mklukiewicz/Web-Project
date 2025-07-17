a222dcccfcc411f1ea146d1bb0eb85a02f1bc185
Marcel_Klukiewicz <Marcel_Klukiewicz@bojkowski.it>
2025-07-17 18:24:11 +0200

    Working pop up,  0 back end

--- ShopTemplate/BazarShop1.0/wwwroot/js/site.js
+++ ShopTemplate/BazarShop1.0/wwwroot/js/site.js
@@ -1,13 +1,21 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
-    const toggleButton = document.getElementById("languageToggle");
-    const dropdown = document.getElementById("languageDropdown");
 
-    toggleButton.addEventListener("click", function (e) {
-        e.stopPropagation(); // nie zamykaj od razu
-        dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+    const languageBtn = document.querySelector(".languageToggle");
+    const popup = document.getElementById("languagePopup");
+    const closeBtn = document.querySelector(".closePopup");
+
+    languageBtn?.addEventListener("click", function () {
+        console.log("Kliknięto globus");
+        popup.classList.add("show");
     });
 
-    document.addEventListener("click", function () {
-        dropdown.style.display = "none";
+    closeBtn?.addEventListener("click", function () {
+        popup.classList.remove("show");
+    });
+
+    popup?.addEventListener("click", function (e) {
+        if (e.target === popup) {
+            popup.classList.remove("show");
+        }
     });
 });
